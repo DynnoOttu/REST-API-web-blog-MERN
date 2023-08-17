a6d583c18910fd1bb82e85f3b599b17112e7e090
DynnoOttu <dynnoottu968@gmail.com>
2023-08-17 12:44:30 +0800

    Memvalidasi Request Body dan Memberikan Error Response yang Dinamis

--- src/controllers/blog.js
+++ src/controllers/blog.js
@@ -1,6 +1,16 @@
+const { validationResult } = require("express-validator");
 exports.createBlogPost = (req, res, next) => {
   const title = req.body.title;
   const body = req.body.body;
+
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    const err = new Error("Invalid Value");
+    err.errorStatus = 400;
+    err.data = errors.array();
+    throw err;
+  }
 
   const result = {
     message: "Create Blog Post Success",
--- src/routes/blog.js
+++ src/routes/blog.js
@@ -1,10 +1,22 @@
 const express = require("express");
+const { body } = require("express-validator");
 
 const router = express.Router();
 
 const blogController = require("../controllers/blog");
 
 // ==> POST : v1/blog/post
-router.post("/post", blogController.createBlogPost);
+router.post(
+  "/post",
+  [
+    body("title")
+      .isLength({ min: 5 })
+      .withMessage("Title harus lebih dari 5 Huruf"),
+    body("body")
+      .isLength({ min: 5 })
+      .withMessage("Body harus lebih dari 5 huruf"),
+  ],
+  blogController.createBlogPost
+);
 
 module.exports = router;
