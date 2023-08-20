b00d96926896d25bc2e4ffc116596c57d65c02fa
DynnoOttu <dynnoottu968@gmail.com>
2023-08-21 00:37:34 +0800

    delete blog post

--- src/controllers/blog.js
+++ src/controllers/blog.js
@@ -1,4 +1,7 @@
 const { validationResult } = require("express-validator");
+const path = require("path");
+const fs = require("fs");
+
 const BlogPost = require("../models/blog");
 exports.createBlogPost = (req, res, next) => {
   const errors = validationResult(req);
@@ -118,4 +121,37 @@ exports.updateBlogPost = (req, res, next) => {
     .catch((err) => {
       next(err);
     });
+};
+
+exports.deleteBlogPost = (req, res, next) => {
+  const postId = req.params.postId;
+
+  BlogPost.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const err = new Error("Invalid Value");
+        err.errorStatus = 400;
+        err.data = errors.array();
+        throw err;
+      }
+      removeImage(post.image);
+      return BlogPost.findByIdAndRemove(postId);
+    })
+    .then((result) => {
+      res.status(200).json({
+        message: "Delete Success",
+        data: result,
+      });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+const removeImage = (filePath) => {
+  console.log("File Path", filePath);
+  console.log("Dir name: ", __dirname);
+
+  filePath = path.join(__dirname, "../../", filePath);
+  fs.unlink(filePath, (err) => console.log(err));
 };
